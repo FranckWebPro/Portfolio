0e47dddec6559945ae3c191d2f3c66ceb0b702e9
Franck <contact@franckwebpro.com>
2024-07-17 16:44:12 +0200

    fix react call

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="../favicon.ico" sizes="any" />
+      </head>
       <body className={`${bG.className} bg-darkColor text-lightColor`}>
         {children}
       </body>

3a17ae92f0a2e5be04e8f76d6ebc6370484350f0
Franck <contact@franckwebpro.com>
2024-07-17 17:02:48 +0200

    fix lint issue

--- src/app/ui/dashboard/headerDashboard.tsx
+++ src/app/ui/dashboard/headerDashboard.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function HeaderDashboard() {
   return (
-    <header className="fixed z-50 mx-auto h-16 w-full animate-blur md:h-20">
+    <header className="animate-blur fixed z-50 mx-auto h-16 w-full md:h-20">
       <nav className="mx-auto flex h-16 max-w-screen-2xl items-center justify-between">
         <div className="md:flex md:items-center md:gap-12">
           <Link className="block text-teal-600 dark:text-teal-600" href="/">
@@ -32,7 +32,7 @@ export default function HeaderDashboard() {
 
         <div className="flex items-center gap-4 sm:flex">
           <a
-            className="rounded-lg bg-secondaryColor px-6 py-3 text-sm font-semibold text-darkColor shadow duration-300 hover:bg-secondaryLight hover:text-lightColor 2xl:text-base"
+            className="bg-secondaryColor text-darkColor hover:bg-secondaryLight hover:text-lightColor rounded-lg px-6 py-3 text-sm font-semibold shadow duration-300 2xl:text-base"
             href="/"
           >
             Déconnexion
@@ -46,13 +46,8 @@ export default function HeaderDashboard() {
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
               >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
+                <path d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </button>
           </div>

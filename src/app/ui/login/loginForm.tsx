11ce18ad2a855120f78036381f3d5aeb084fdabb
Franck <contact@franckwebpro.com>
2024-07-19 03:13:12 +0200

    fix linter

--- src/app/ui/login/loginForm.tsx
+++ src/app/ui/login/loginForm.tsx
@@ -2,22 +2,13 @@
 import React from "react";
 
 import {
-  ArrowRightIcon,
   AtSymbolIcon,
-  ExclamationCircleIcon,
   KeyIcon,
 } from "@heroicons/react/24/outline";
-import { useActionState } from "react";
-import { authenticate } from "@/app/lib/actions";
 
 export default function LoginForm() {
-  const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
-    undefined
-  );
-
   return (
-    <form action={formAction} className="space-y-3">
+    <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`mb-3 text-2xl`}>Please log in to continue.</h1>
         <div className="w-full">
@@ -61,16 +52,16 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <button className="mt-4 w-full" aria-disabled={isPending}>
+        {/* <button className="mt-4 w-full" aria-disabled={isPending}>
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </button>
+        </button> */}
         <div className="flex h-8 items-end space-x-1">
-          {errorMessage && (
+          {/* {errorMessage && (
             <>
               <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
               <p className="text-sm text-red-500">{errorMessage}</p>
             </>
-          )}
+          )} */}
         </div>
       </div>
     </form>

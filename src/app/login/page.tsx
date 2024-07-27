22262adbf664a0493c1a63e29740cb2f1f1ab380
Franck <contact@franckwebpro.com>
2024-07-27 11:51:27 +0200

    removed login for now

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import HeaderDashboard from "../ui/dashboard/headerDashboard";
-import AuthButton from "../ui/login/authButton.server";
 
 export default function LoginPage() {
   return (
@@ -9,7 +8,6 @@ export default function LoginPage() {
       <main className="flex min-h-screen items-center justify-center">
         <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
           <div className="w-32 text-lightColor md:w-36"></div>
-          <AuthButton />
         </div>
       </main>
     </>

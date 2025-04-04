8c5ea05a4f4a62503845883c8c45e5a586dc410d
Franck <contact@franckwebpro.com>
2025-04-04 12:46:53 +0200

    remove blog from en

--- src/app/ui/dashboard/headerDashboard.tsx
+++ src/app/ui/dashboard/headerDashboard.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import logo from "../images/logoLight.png";
+import logo from "@/assets/images/logoLight.png";
 import { createClient } from "@/lib/supabase/client";
 
 export default function HeaderDashboard() {
